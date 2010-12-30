c87614a8b277c84dfa6ccee84b1d2e71d8887a1a
Bogdan Cristea <cristeab@gmail.com>
2010-12-30 19:59:35 +0200

    itload not yet complete

--- pyitpp.py
+++ pyitpp.py
@@ -1,14 +1,14 @@
 # File:   pyitpp.py
-# Brief:  Loads an IT++ itfile content and outputs a dictionary with all found variables
+# Brief:  Loads an IT++ itfile content and outputs a dictionary with all found variables.
 # Author: Bogdan Cristea
 #
 # Usage: from pyitpp import itload
 #        out = itload('fname.it')
 #
 # This module provides a function for loading itfile content into matrices/scalars
-# and outputs all these variables as a dictionary whose keys are variable names.
-# All required functions/classes from python modules (numpy, sys, os, stat, struct)
-# are loaded, be aware about possible name clashes (e.g. mat) 
+# and outputs all these variables as a dictionary whose keys are variable names as
+# found in itfile. This module uses numpy module for matrix operations.
+# Thus the provided functionality is similar to itload() function from MATLAB.
 #
 # -------------------------------------------------------------------------
 #
@@ -39,6 +39,7 @@ from stat import ST_SIZE
 from struct import unpack
 from numpy import mat
 from numpy import reshape
+from numpy import zeros
 
 def __fgetstr(fid):
     str = ''
@@ -93,10 +94,83 @@ def itload(in_file):
         
         if len(var_type) == 0: #A deleted entry -> skip it
             pass
+        #scalars
+        # --- bin ---
+        elif 'bin' == var_type:
+            out[var_name] = unpack('b', f.read(1))[0]
+        # --- int8 (char) ---
+        elif 'int8' == var_type:
+            out[var_name] = unpack('c', f.read(1))[0]
+        # --- int16 (short) ---
+        elif 'int16' == var_type:
+            out[var_name] = unpack('h', f.read(2))[0]
+        # --- int32 (int) ---
+        elif 'int32' == var_type:
+            out[var_name] = unpack('i', f.read(4))[0]
+        # --- float32 (float) ---
+        elif 'float32' == var_type:
+            out[var_name] = unpack('f', f.read(4))[0]
+        # --- float64 (double) ---
+        elif 'float64' == var_type:
+            out[var_name] = unpack('d', f.read(8))[0]
+        # --- cfloat32 (complex<float>) ---
+        elif 'cfloat32' == var_type:
+            real_imag = unpack('2f', f.read(8))
+            out[var_name] = complex(real_imag[0], real_imag[1])
+        # --- cfloat64 (complex<double>) ---
+        elif 'cfloat64' == var_type:
+            real_imag = unpack('2d', f.read(16))
+            out[var_name] = complex(real_imag[0], real_imag[1])
+        
+        #vectors
+        # --- bvec ---
+        elif 'bvec' == var_type:
+            length = unpack('L', f.read(8))[0]
+            fmt = str(length)+'b'
+            out[var_name] = mat(unpack(fmt, f.read(length)), 'uint8').T#convert to a column vector
+        # --- string ---
+        elif 'string' == var_type:
+            length = unpack('L', f.read(8))[0]
+            fmt = str(length)+'c'
+            out[var_name] = "".join(unpack(fmt, f.read(length)))
+        # --- svec ---
+        elif 'svec' == var_type:
+            length = unpack('L', f.read(8))[0]
+            fmt = str(length)+'h'
+            out[var_name] = mat(unpack(fmt, f.read(length*2)), 'int16').T#convert to a column vector
+        # --- ivec ---
+        elif 'ivec' == var_type:
+            length = unpack('L', f.read(8))[0]
+            fmt = str(length)+'i'
+            out[var_name] = mat(unpack(fmt, f.read(length*4)), 'int32').T#convert to a column vector
+        # --- fvec ---
+        elif 'fvec' == var_type:
+            length = unpack('L', f.read(8))[0]
+            fmt = str(length)+'f'
+            out[var_name] = mat(unpack(fmt, f.read(length*4)), 'float32').T#convert to a column vector
+        # --- dvec ---
         elif 'dvec' == var_type:
             length = unpack('L', f.read(8))[0]
             fmt = str(length)+'d'
             out[var_name] = mat(unpack(fmt, f.read(length*8)), 'float64').T#convert to a column vector
+        # --- fcvec ---
+        elif 'fcvec' == var_type:
+            length = unpack('L', f.read(8))[0]
+            fmt = str(2*length)+'f'
+            real_imag = mat(unpack(fmt, f.read(2*length*4)), 'float32').T#convert to a column vector
+            out[var_name] = zeros((length, 1));
+            for i in range(length):
+                out[var_name][i,0] = complex(real_imag[2*i], real_imag[2*i+1])
+        # --- dcvec ---
+        elif 'dcvec' == var_type:
+            length = unpack('L', f.read(8))[0]
+            fmt = str(2*length)+'d'
+            real_imag = mat(unpack(fmt, f.read(2*length*8)), 'float64').T#convert to a column vector
+            out[var_name] = zeros((length, 1), complex);
+            for i in range(length):
+                out[var_name][i,0] = complex(real_imag[2*i], real_imag[2*i+1])        
+        
+        #matrices
         elif 'dmat' == var_type:
             rows = unpack('L', f.read(8))[0]
             cols = unpack('L', f.read(8))[0]
--- test_pyitpp.py
+++ test_pyitpp.py
@@ -1,9 +1,61 @@
 #!/usr/bin/env python
 
-import pyitpp as itpp
+from pyitpp import itload
+import unittest
+from numpy import mat
 
-in_file = '/home/bogdan/C++/IT++_trial/pccc_bersim_awgn.it'
-out = itpp.itload(in_file)
-print out.keys()
-for key in out.keys():
-    print out[key].shape
+class TestITLoad(unittest.TestCase):
+
+    def setUp(self):
+        self.out = itload('../C++/IT++_trial/test_pyitpp.it')
+        self.eps = 1e-6
+
+    def test_scalars(self):
+        # test all supported scalars for type and value
+        self.assertEqual(type(self.out['a']).__name__, 'int')
+        self.assertEqual(self.out['a'], 1)
+        
+        self.assertEqual(type(self.out['b']).__name__, 'str')
+        self.assertEqual(self.out['b'], '2')
+        
+        self.assertEqual(type(self.out['c']).__name__, 'int')
+        self.assertEqual(self.out['c'], -3)
+        
+        self.assertEqual(type(self.out['d']).__name__, 'int')
+        self.assertEqual(self.out['d'], -4)
+        
+        self.assertEqual(type(self.out['e']).__name__, 'float')
+        self.assertTrue(abs(self.out['e']-5.67) < self.eps)
+        
+        self.assertEqual(type(self.out['f']).__name__, 'float')
+        self.assertTrue(abs(self.out['f']-8.91234567) < self.eps)
+        
+        self.assertEqual(type(self.out['g']).__name__, 'complex')
+        self.assertTrue(abs(self.out['g']-complex(-1.234, 4.321)) < self.eps)
+        
+        self.assertEqual(type(self.out['h']).__name__, 'complex')
+        self.assertTrue(abs(self.out['h']-complex(-1.234567, 4.321654)) < self.eps)
+
+    def test_vectors(self):
+        # test all supported vectors for type and value
+        self.assertEqual(type(self.out['i']).__name__, 'matrix')
+        self.assertTrue(all(self.out['i'] == mat('0; 1; 1; 0; 0; 1')))
+        
+        self.assertEqual(type(self.out['j']).__name__, 'str')
+        self.assertEqual(self.out['j'], 'abc')
+        
+        self.assertEqual(type(self.out['k']).__name__, 'matrix')
+        self.assertTrue(all(self.out['k'] == mat('10; 11; 12; 13; 14; 15; 16; 17; 18; 19')))
+        
+        self.assertEqual(type(self.out['l']).__name__, 'matrix')
+        self.assertTrue(all(self.out['l'] == mat('20; 21; 22; 23; 24; 25; 26; 27; 28; 29')))
+        
+        self.assertEqual(type(self.out['n']).__name__, 'ndarray')
+        self.assertTrue(all(abs(self.out['n']-mat('30; 31; 32; 33; 34; 35; 36; 37; 38; 39')) < self.eps))
+        print self.out['n']
+        
+        self.assertEqual(type(self.out['o']).__name__, '')
+        self.assertTrue(all(abs(self.out['o']-range(complex(0, 0), complex(0.1, -0.1), complex(0.01, -0.01))) < self.eps))
+
+if __name__ == '__main__':
+    unittest.main()
